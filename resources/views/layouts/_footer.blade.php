9ef8c62b2632422c659384051d68eaf0163dfed1
vagrant <vagrant@homestead>
2019-03-27 06:24:46 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="foot" style="position: absolute;bottom: 0;width: 100%; height: 60px;background-color: #000;">
+    <div class="container" style="padding-right: 15px;padding-left: 15px;">
+        <p class="float-left" style=" margin: 19px 0;color: #c1c1c1;">
+            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank" style="color: inherit;">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right" style=" margin: 19px 0;color: #c1c1c1;"><a href="mailto:name@email.com" style="color: inherit;">联系我们</a></p>
+    </div>
+</footer>
