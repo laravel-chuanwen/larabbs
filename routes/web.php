c9f7dd826ffdfcc297f06959a6ae784342dfc07e
vagrant <vagrant@homestead>
2019-04-08 12:08:50 +0000

    生成用户认证代码

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,10 @@
 <?php
 
 Route::get('/','PagesController@root');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
