913b7610aaea62096256d76be5617f72bbdb6def
vagrant <vagrant@homestead>
2019-05-09 16:25:18 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,7 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
