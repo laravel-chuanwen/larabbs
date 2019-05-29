6cdf012d9742edba4eebd4020551817f2f6cab10
vagrant <vagrant@homestead>
2019-05-29 08:37:24 +0000

    Topic index page

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="foot" style="position: absolute;bottom: 0;width: 100%; height: 60px;background-color: #000;">
+<footer class="foot" style="width: 100%; height: 60px;background-color: #000;">
     <div class="container" style="padding-right: 15px;padding-left: 15px;">
         <p class="float-left" style=" margin: 19px 0;color: #c1c1c1;">
             由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank" style="color: inherit;">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
